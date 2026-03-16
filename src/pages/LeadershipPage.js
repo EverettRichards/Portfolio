f0712690183112838c0ff7e2df221cc8fdd7cecd
EverettRichards <ehrichards9@gmail.com>
2026-03-16 09:42:46 -0700

    Added mention of CTRL

--- src/pages/LeadershipPage.js
+++ src/pages/LeadershipPage.js
@@ -3,18 +3,39 @@ import React, { useEffect } from "react";
 
 const leadershipData = [
   {
-    title: "President of the <a class='underline' href='https://acm.sdsu.edu' target='_blank'>ACM @ SDSU</a>",
-    body: "Founder of the Association for Computing Machinery (ACM) Student Chapter. Lead workshops, organize tech talks, and expanded membership engagement. Planned, implemented, and judged projects for the 2025 <a class='underline' href='https://acm.sdsu.edu/innovate4sdsu' target='_blank'>Innovate 4 SDSU Hackathon.</a>",
+    title: "Founder & President",
+    org: "Association for Computing Machinery (ACM) at SDSU",
+    link: "https://acm.sdsu.edu",
+    body: `
+    Organized and presented 30+ workshops on topics including machine learning, web development, version control, prompt engineering, and Python programming.
+    Planned and implemented recurring social events, including the STEM Karaoke Night series and biannual Computer Science Bowling Night.
+    Managed an operating budget of more than $6,000 per year, including $2,500 from Google.`,
     image: require("../images/hackathon_judge.jpg"),
   },
   {
+    title: "Co-Founder & Vice President",
+    org: "Coalition of Tech Representatives and Leadership (CTRL)",
+    link: "https://ctrl.sdsu.edu",
+    body: `Supervised a wide range of tech-oriented career development activities, including the
+    <a href="https://acm.sdsu.edu/hack" target="_blank" class="underline">Innovate4SDSU Hackathon</a> (150+ participants),
+    <a href="https://ctrl.sdsu.edu/#/nxp" target="_blank" class="underline">NXP Engineering Bootcamp</a> (10-week industry collaboration), and
+    <a href="https://ctrl.sdsu.edu/#/codeblitz" target="_blank" class="underline">CodeBlitz</a> (fast-paced programming competition).
+    Chaired the <a href="https://ctrl.sdsu.edu/#/icc" target="_blank" class="underline">Inter-Club Council</a> of representatives from a dozen tech organizations at SDSU.
+    Developed and maintained the organization's <a href="https://ctrl.sdsu.edu" target="_blank" class="underline">website</a>.`,
+    image: require("../images/ctrlhack.jpeg"),
+  },
+  {
     title: "University Council Representative",
-    body: "Represented College of Sciences students on the Associated Students University Council. Advocated for sustainability and STEM research funding. Serve as an Executive Officer on the College of Sciences Student Council.",
+    org: "Associated Students of SDSU + College of Sciences Student Council",
+    link: "https://as.sdsu.edu/",
+    body: "Represented nearly 8,000 College of Sciences students on the Associated Students University Council. Met with university officials (including SDSU President Adela de la Torre, pictured on the right) to advocate for student needs. Led a push for transparency in mandatory student fees. Served as an Executive Officer on the College of Sciences Student Council.",
     image: require("../images/adela1.jpg"),
   },
   {
     title: "Math & Computer Science Tutoring",
-    body: "Served as a tutor at the Math and Science Learning Center (MSLC), and an Instructional Assistant for Discrete Mathematics. Tutored physics, computer science, probability, linear algebra, and real analysis.",
+    org: "SDSU Math and Science Learning Center (MSLC)",
+    link: "https://mslc.sdsu.edu",
+    body: "Served as a tutor at the Math and Science Learning Center (MSLC), and an Instructional Assistant for Discrete Mathematics. Tutored hundreds of students in physics, computer science, probability, linear algebra, discrete mathematics, and real analysis.",
     image: require("../images/mslc.jpeg"),
   },
 ];
@@ -48,9 +69,17 @@ export default function LeadershipPage() {
             {/* Text Section */}
             <div className="flex-1 md:my-auto text-center md:text-left">
               <h3 
-                className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white mb-4 text-center md:text-left" 
+                className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white mb-2 text-center md:text-left" 
                 dangerouslySetInnerHTML={{ __html: item.title }}
               ></h3>
+              <h3 
+                className="text-lg md:text-xl font-bold text-gray-800 dark:text-blue-300 mb-2 text-center md:text-left">
+                <a
+                  href={ item.link }
+                  target='_blank'
+                  dangerouslySetInnerHTML={{ __html: item.org }}
+                ></a>
+              </h3>
               <p 
                 className="text-gray-700 dark:text-gray-400 text-base md:text-lg lg:text-xl" 
                 dangerouslySetInnerHTML={{ __html: item.body }}
